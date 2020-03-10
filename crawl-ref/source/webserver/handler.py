8f4132b9673f59e89d53c315f25cb4a0baef75cc
Aidan Holm <aidanholm@gmail.com>
2020-03-10 21:06:44 +0800

    Add authentication handler mixins

--- crawl-ref/source/webserver/handler.py
+++ crawl-ref/source/webserver/handler.py
@@ -1,6 +1,30 @@
 import tornado.web
 import tornado.websocket
 
+import auth
+import userdb
+
+
+class AuthRequiredMixin():
+    """Mixin for RequestHandlers requiring user login."""
+
+    def get_current_user(self):
+        cookie = self.get_cookie('login', '').replace('%20', ' ')
+        username, ok = auth.check_login_cookie(cookie)
+        if not ok:
+            raise tornado.web.HTTPError(401)
+        return userdb.get_user_info(username)
+
+
+class AdminOnlyMixin(AuthRequiredMixin):
+    """Mixin for RequestHandlers accessible to administrators only."""
+
+    def get_current_user(self):
+        user = super(AdminOnlyMixin, self).get_current_user()
+        if not user.is_admin:
+            raise tornado.web.HTTPError(403)
+        return user
+
 
 class RequestHandler(tornado.web.RequestHandler):
     pass
